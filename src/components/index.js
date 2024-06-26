2b62c4a52926474af3dcb2f4c084e2687d4a194c
Majid0899 <Majid0899@gmail.com>
2024-06-26 16:45:33 +0530

    version 1

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
+import useTodo from "../context/TodoContext";
+
+import { TodoContext,TodoProvider} from "../context/TodoContext";
+
+
+export {TodoForm,TodoItem,TodoContext,TodoProvider,useTodo}
